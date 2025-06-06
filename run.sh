035e5eb896300f18eb51d8776f63710cd0bbed06
ntnn19 <natannagar89@gmail.com>
2025-06-07 01:30:28 +0200

    added run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+python ../../../../../workflows/alphafold3_workflow/workflow/scripts/prepare_workflow.py config/config.yaml
+snakemake --configfile config/config.yaml \
+--use-singularity --singularity-args  \
+"--nv -B $(pwd)/output:/root/af_output -B $(pwd)/workflow/scripts/match_template_and_target_chain_ids.py:/app/utils/chain_pair_generator.py -B $(realpath ../../../../../af2db/AlphaFold):/AF_data" \
+-j 300 -c all \
+-p -k -w 30 --rerun-triggers mtime --workflow-profile profile --workflow-profile profile
